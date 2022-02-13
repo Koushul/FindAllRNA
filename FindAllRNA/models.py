3a2d1f306a7b451b9196858e3f5a7ea685a1a1e1
Koushul Ramjattun <kor11@pitt.edu>
2022-02-12 21:30:53 -0500

    created encoders and CNN models

---
+++ FindAllRNA/models.py
@@ -0,0 +1,150 @@
+import os
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+
+import tensorflow as tf
+from tensorflow import keras
+
+from tensorflow.keras.layers import LeakyReLU
+import logging
+logging.getLogger('tensorflow').setLevel(logging.ERROR)
+
+# workaround to solve keras __name__ issue
+LR = LeakyReLU()
+LR.__name__ = 'relu'
+
+
+def buildCNNModel(inshape, num_classes, nlayers=2, cnndim=2):
+    n = 32
+    model = keras.Sequential()
+    for i in range(nlayers):
+        if (i==0):
+            if (cnndim==2):
+                model.add(keras.layers.Conv2D(n*(2**(i)),(3),padding='same',input_shape=inshape, activation=LR))
+            else:
+                model.add(keras.layers.Conv1D(n*(2**(i)),(3),padding='same',input_shape=inshape, activation=LR))
+        else:
+            if (cnndim==2):
+                model.add(keras.layers.Conv2D(n*(2**(i)),(3),padding='same',activation=LR))
+            else:
+                model.add(keras.layers.Conv1D(n*(2**(i)),(3),padding='same',activation=LR))
+        if (cnndim==2):
+            model.add(keras.layers.MaxPooling2D(2))
+        else:
+            model.add(keras.layers.MaxPooling1D(2))
+        model.add(keras.layers.Dropout(rate=0.25))
+        
+    if (nlayers>0):
+        model.add(keras.layers.Flatten())
+    else:
+        model.add(keras.layers.Dense(500, input_shape=inshape, activation=LR))
+
+    model.add(keras.layers.Dense(1000, activation=LR))
+    model.add(keras.layers.Dense(100, activation=LR))
+    model.add(keras.layers.Dropout(rate=0.5))
+    model.add(keras.layers.Dense(num_classes, activation=tf.nn.softmax))
+    return model
+
+def buildCNNModelImproved1D(inshape,num_classes):
+
+    model = keras.Sequential()
+    
+    model.add(keras.layers.Conv1D(128,10,padding='same',input_shape=inshape))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU(alpha=0.5))
+    model.add(keras.layers.MaxPooling1D(2))
+
+    model.add(keras.layers.Conv1D(128,10,padding='same'))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU(alpha=0.5))
+    model.add(keras.layers.MaxPooling1D(4))
+
+    model.add(keras.layers.GaussianNoise(0.3))
+    
+    model.add(keras.layers.Conv1D(256,10,padding='same'))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU(alpha=0.5))
+    model.add(keras.layers.MaxPooling1D(2))
+
+    model.add(keras.layers.Conv1D(256,10,padding='same'))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU(alpha=0.5))
+    model.add(keras.layers.MaxPooling1D(4))
+
+    model.add(keras.layers.GaussianNoise(0.3))
+    
+    model.add(keras.layers.Conv1D(256,10,padding='same'))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU(alpha=0.5))
+    model.add(keras.layers.MaxPooling1D(4))
+    model.add(keras.layers.GaussianNoise(0.3))
+    
+    model.add(keras.layers.Dropout(rate=0.2))
+    
+    model.add(keras.layers.Flatten())
+
+    model.add(keras.layers.Dense(128))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU(alpha=0.5))
+    
+    model.add(keras.layers.Dense(64))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU(alpha=0.5))
+
+    model.add(keras.layers.Dense(num_classes, activation=tf.nn.softmax))
+    return model
+
+def buildCNNModelImproved2D(inshape,num_classes):
+
+    model = keras.Sequential()
+    
+    model.add(keras.layers.Conv2D(128,(3,3),padding='same',input_shape=inshape,
+                                 kernel_regularizer=keras.regularizers.l2(0.003)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU(alpha=0.5))
+
+    model.add(keras.layers.Conv2D(128,(3,3),padding='same'))   
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU(alpha=0.5))
+    model.add(keras.layers.MaxPooling2D((2,2)))
+
+    model.add(keras.layers.GaussianNoise(0.3))
+    
+    model.add(keras.layers.Conv2D(256,(3,3),padding='same'))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU(alpha=0.5))
+    
+    model.add(keras.layers.Conv2D(256,(3,3),padding='same'))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU(alpha=0.5))
+    model.add(keras.layers.MaxPooling2D((2,2)))
+
+    model.add(keras.layers.GaussianNoise(0.3))
+    
+    model.add(keras.layers.Conv2D(256,(3,3),padding='same'))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU(alpha=0.5))
+    model.add(keras.layers.MaxPooling2D((2,2)))
+    model.add(keras.layers.GaussianNoise(0.3))
+    
+    model.add(keras.layers.Dropout(rate=0.2))
+    
+    model.add(keras.layers.Flatten())
+
+    model.add(keras.layers.Dense(128))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU(alpha=0.5))
+    
+    model.add(keras.layers.Dense(64))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU(alpha=0.5))
+
+    model.add(keras.layers.Dense(num_classes, activation=tf.nn.softmax))
+    return model
+
+
+if __name__ == '__main__':
+    m = buildCNNModel(inshape=(200, 16),num_classes=3, nlayers=2, cnndim=1)
+    print(m.summary())
---
+++ FindAllRNA/utils.py
@@ -0,0 +1,38 @@
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+
+
+def get_labels(fasta_file: str) -> list:
+    seq_rec = list(SeqIO.parse(fasta_file, "fasta"))
+    labels = [r.name for r in seq_rec]
+    return labels
+
+def write_seqs(fasta_file: str, seqs: list, ids:list, desc: str = ''):
+    seqrecords = []
+    for i, s in enumerate(seqs):
+        sr = SeqRecord(s, id=ids[i], description=desc)
+        seqrecords.append(sr)
+    SeqIO.write(seqrecords, fasta_file, 'fasta')
+    
+class DataLoader(object):
+    
+    def __init__(self, x_train: str, x_test: str, x_val: str):
+        self.x_train = x_train
+        self.x_test = x_test
+        self.x_val = x_val
+
+        self.train_labels = get_labels(self.x_train)
+        self.val_labels = get_labels(self.x_val)
+        self.test_labels = get_labels(self.x_test)
+        
+    
+    # def
+    #     bn = 25
+    #     seqTrain = get_seqs_with_bnoise(fastaTrain,nperc=bn)
+    #     seqVal = get_seqs_with_bnoise(fastaVal,nperc=bn)
+    #     seqTest = get_seqs_with_bnoise(fastaTest,nperc=bn)
+            
+            
+        
+        
+        
